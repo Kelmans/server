ee3e043a6740adfdb1c2e562712f1ac0fa27178c
Vladislav <32076727+Kelmans@users.noreply.github.com>
2024-03-22 17:52:32 +0500

    add mongo

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { MongoClient } from 'mongodb';
 
+const mongoClient = new MongoClient('mongodb://127.0.0.1:27017/');
+async function run() {
+  try {
+    // Подключаемся к серверу
+    await mongoClient.connect();
+    console.log('Подключение установлено');
+    const db = mongoClient.db('test');
+    const collection = db.collection('users');
+    const count = await collection.countDocuments();
+    console.log(count, 'count');
+  } catch (err) {
+    console.log(err);
+  } finally {
+    // Закрываем подключение при завершении работы или при ошибке
+    await mongoClient.close();
+    console.log('Подключение закрыто');
+  }
+}
+run().catch(console.log);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Тут URL, на котором крутится фронтенд приложение
+    origin: 'http://localhost:8080', // Тут URL, на котором крутится фронтенд приложение
     methods: 'GET,POST',
     allowedHeaders: 'Content-Type,Authorization',
   });
